70ff3966ff2760e7bfadd996dc6bff6ebba613ee
Laura Berge <lauraeberge@gmail.com>
2020-05-06 14:56:25 -0500

    adds the scripts needed to change a wallpaper on Mac

--- wallpapers.py
+++ wallpapers.py
@@ -3,6 +3,13 @@ import time
 
 def change_wallpaper(image):
     # change wallpaper on mac given an image
+    command = """/usr/bin/osascript<<END
+    tell application "Finder"
+        set desktop picture to POSIX file "%s"
+    end tell
+    END"""
+    subprocess.Popen(command%image, shell=True)
+    subprocess.call(["killall dock"], shell=True)
 
 def main():
     # automate change_wallpaper function
