d7ac5377e2c302f2d38b8ba09c1fb82f1a8060fe
Laura Berge <lauraeberge@gmail.com>
2020-05-06 14:48:55 -0500

    first commit

---
+++ wallpapers.py
@@ -0,0 +1,11 @@
+import subprocess
+import time
+
+def change_wallpaper(image):
+    # change wallpaper on mac given an image
+
+def main():
+    # automate change_wallpaper function
+
+while True:
+    main()
